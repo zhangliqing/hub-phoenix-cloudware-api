35da232384c636edbe907aca26aada58b0bfd7a2
zhangliqing <623009470@qq.com>
2017-10-17 12:29:41 +0800

    test

--- database.js
+++ database.js
@@ -8,7 +8,7 @@ var db=mongoose.connection;
 
 db.on('error',console.error.bind(console,'connection error: '));
 db.once('open',function () {
-  console.log('Connected to mongodb.')
+  console.log('Connected  to mongodb.')
 });
 
 var instanceSchema = mongoose.Schema({
