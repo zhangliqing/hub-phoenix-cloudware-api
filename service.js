18a1e01fae38ac4eb620a0d679bb557643cdcf42
zhangliqing <623009470@qq.com>
2017-11-08 17:51:08 +0800

    rm lb

--- service.js
+++ service.js
@@ -3,7 +3,8 @@
  */
 exports = {
   rancher: {
-    endpoint: process.env.RANCHER_ENDPOINT,//去单引号
+    endpoint: process.env.RANCHER_ENDPOINT,
+    wsaddr:process.env.WSADDR,
     user: process.env.RANCHER_USER,
     pass: process.env.RANCHER_PASS,
     env:process.env.RANCHER_ENV
