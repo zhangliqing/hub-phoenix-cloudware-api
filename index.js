90d7b05286acb424a15285fc6e4515b915c5b0a7
zhangliqing <623009470@qq.com>
2017-11-09 14:23:35 +0800

    test

--- index.js
+++ index.js
@@ -39,118 +39,123 @@ console.log(service.rancher.env)
 //创建用户对应文件夹
 //req.body user_id
 router.route('/volumes').post(function(req, res) {
-  console.log('recive post to /volumes')
-  var data = {
-    "type": "volume",
-    "driver": "rancher-nfs",
-    "name": req.body.userId,
-    "driverOpts": {}
-  }
-  var openContainer = function(user_id) {
-    var tmpData = {
-      "instanceTriggeredStop": "stop",
-      "startOnCreate": true,
-      "publishAllPorts": false,
-      "privileged": false,
-      "stdinOpen": true,
-      "tty": true,
-      "readOnly": false,
-      "runInit": false,
-      "networkMode": "managed",
-      "type": "container",
-      "requestedHostId": "1h5",
-      "secrets": [],
-      "dataVolumes": [
-        user_id + ":/data"
-      ],
-      "dataVolumesFrom": [],
-      "dns": [],
-      "dnsSearch": [],
-      "capAdd": [],
-      "capDrop": [],
-      "devices": [],
-      "logConfig": {
-        "driver": "",
-        "config": {}
-      },
-      //"cmd":["echo","1"],
-      "dataVolumesFromLaunchConfigs": [],
-      "imageUuid": "docker:busybox",
-      "ports": [],
-      "instanceLinks": {},
-      "labels": {"container_type": "cloudware"},
-      "name": "test" + user_id,
-      "count": null,
-      "createIndex": null,
-      "created": null,
-      "deploymentUnitUuid": null,
-      "description": null,
-      "externalId": null,
-      "firstRunning": null,
-      "healthState": null,
-      "hostname": null,
-      "kind": null,
-      "memoryReservation": null,
-      "milliCpuReservation": null,
-      "removed": null,
-      "startCount": null,
-      "uuid": null,
-      "volumeDriver": null,
-      "workingDir": null,
-      "user": null,
-      "domainName": null,
-      "memorySwap": null,
-      "memory": null,
-      "cpuSet": null,
-      "cpuShares": null,
-      "pidMode": null,
-      "blkioWeight": null,
-      "cgroupParent": null,
-      "usernsMode": null,
-      "pidsLimit": null,
-      "diskQuota": null,
-      "cpuCount": null,
-      "cpuPercent": null,
-      "ioMaximumIOps": null,
-      "ioMaximumBandwidth": null,
-      "cpuPeriod": null,
-      "cpuQuota": null,
-      "cpuSetMems": null,
-      "isolation": null,
-      "kernelMemory": null,
-      "memorySwappiness": null,
-      "shmSize": null,
-      "uts": null,
-      "ipcMode": null,
-      "stopSignal": null,
-      "oomScoreAdj": null,
-      "ip": null,
-      "ip6": null,
-      "healthInterval": null,
-      "healthTimeout": null,
-      "healthRetries": null
-    }
-    request.post({
-      url: service.rancher.endpoint + '/projects/1a3504/container',
-      json: tmpData
-    })
-  }
 
-  rp({
-    method: 'POST',
-    uri: service.rancher.endpoint + '/projects/' + service.rancher.env + '/volume',
-    body: data,
-    json: true
-  })
-    .then(function() {
-      openContainer(req.body.userId)
-      console.log('create volume success')
-      res.send(201, {errorCode: 0})
-    })
-    .catch(function(err) {
+  // var data = {
+  //   "type": "volume",
+  //   "driver": "rancher-nfs",
+  //   "name": req.body.userId,
+  //   "driverOpts": {}
+  // }
+  var user_id = req.body.userId
+
+  var tmpData = {
+    "instanceTriggeredStop": "stop",
+    "startOnCreate": true,
+    "publishAllPorts": false,
+    "privileged": false,
+    "stdinOpen": true,
+    "tty": true,
+    "readOnly": false,
+    "runInit": false,
+    "networkMode": "managed",
+    "type": "container",
+    "requestedHostId": "1h5",
+    "secrets": [],
+    "dataVolumes": [
+      user_id + ":/data"
+    ],
+    "dataVolumesFrom": [],
+    "dns": [],
+    "dnsSearch": [],
+    "capAdd": [],
+    "capDrop": [],
+    "devices": [],
+    "logConfig": {
+      "driver": "",
+      "config": {}
+    },
+    //"cmd":["echo","1"],
+    "dataVolumesFromLaunchConfigs": [],
+    "imageUuid": "docker:busybox",
+    "ports": [],
+    "instanceLinks": {},
+    "labels": {"container_type": "cloudware"},
+    "name": "test" + user_id,
+    "count": null,
+    "createIndex": null,
+    "created": null,
+    "deploymentUnitUuid": null,
+    "description": null,
+    "externalId": null,
+    "firstRunning": null,
+    "healthState": null,
+    "hostname": null,
+    "kind": null,
+    "memoryReservation": null,
+    "milliCpuReservation": null,
+    "removed": null,
+    "startCount": null,
+    "uuid": null,
+    "volumeDriver": null,
+    "workingDir": null,
+    "user": null,
+    "domainName": null,
+    "memorySwap": null,
+    "memory": null,
+    "cpuSet": null,
+    "cpuShares": null,
+    "pidMode": null,
+    "blkioWeight": null,
+    "cgroupParent": null,
+    "usernsMode": null,
+    "pidsLimit": null,
+    "diskQuota": null,
+    "cpuCount": null,
+    "cpuPercent": null,
+    "ioMaximumIOps": null,
+    "ioMaximumBandwidth": null,
+    "cpuPeriod": null,
+    "cpuQuota": null,
+    "cpuSetMems": null,
+    "isolation": null,
+    "kernelMemory": null,
+    "memorySwappiness": null,
+    "shmSize": null,
+    "uts": null,
+    "ipcMode": null,
+    "stopSignal": null,
+    "oomScoreAdj": null,
+    "ip": null,
+    "ip6": null,
+    "healthInterval": null,
+    "healthTimeout": null,
+    "healthRetries": null
+  }
+  request.post({
+    url: service.rancher.endpoint + '/projects/1a3504/container',
+    json: tmpData
+  }, function(err, httpResponse, body) {
+    if (err) {
       console.log('create volume fialed')
       res.send(500, JSON.stringify({errorCode: 1, errorMessage: 'post to rancher error.'}))
-    })
+    } else {
+      console.log('create volume success')
+      res.send(201, {errorCode: 0})
+    }
+  })
+
+  // rp({
+  //   method: 'POST',
+  //   uri: service.rancher.endpoint + '/projects/' + service.rancher.env + '/volume',
+  //   body: data,
+  //   json: true
+  // })
+  //   .then(function() {
+  //
+  //   })
+  //   .catch(function(err) {
+  //
+  //   })
 })
 
 //启动云件
@@ -425,7 +430,7 @@ router.route('/services').post(function(req, res) {
                       "targetPort": 5678
                     })
 
-                    if(proxyData.launchConfig.ports.indexOf("83:83/tcp") === -1){
+                    if (proxyData.launchConfig.ports.indexOf("83:83/tcp") === -1) {
                       proxyData.launchConfig.ports.push("83:83/tcp")
                     }
 
